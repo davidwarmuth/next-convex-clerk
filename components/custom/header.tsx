5d5ce2ab57bb0f470b86f10deb2a6100de0254b4
InVindicus <david-warmuth@web.de>
2025-02-18 23:29:20 +0100

    created header

---
+++ components/custom/header.tsx
@@ -0,0 +1,35 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "./theme-toggle";
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className="py-4 border-b">
+      <nav className="container flex gap-2 justify-between">
+        <div className="flex gap-10 items-center">
+          <h1 className="font-semibold">
+            <Link href="/">Next-Convex-Clerk</Link>
+          </h1>
+          <ul className="hidden md:flex gap-10 text-sm font-medium">
+            <li>
+              <Link href="/protected/server">Link</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="hidden md:flex gap-4">
+          <ThemeToggle />
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button>Sign in</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+        <div className="md:hidden">mobile menu</div>
+      </nav>
+    </header>
+  );
+}
