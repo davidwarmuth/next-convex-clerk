2c129da312d7ae877114d77fe29753bfc9ff1b85
InVindicus <david-warmuth@web.de>
2025-02-18 23:23:34 +0100

    customized footer

--- components/custom/footer.tsx
+++ components/custom/footer.tsx
@@ -5,7 +5,8 @@ export default function Footer() {
     <footer className="py-4 border-t">
       <div className="container flex flex-col items-center justify-between gap-x-3 gap-y-1 text-center text-sm text-muted-foreground sm:flex-row">
         <p>
-          NSC Template &copy;{new Date().getFullYear()}. All rights reserved.
+          Next-Convex-Clerk &copy;{new Date().getFullYear()}. All rights
+          reserved.
         </p>
         <p className="text-xs">
           Developed by{" "}
